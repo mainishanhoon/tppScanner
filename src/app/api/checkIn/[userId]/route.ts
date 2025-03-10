fac22366eca4c634d345bbc171f6ae2f46366ab6
mainishanhoon <NishanKashyap@hotmail.com>
2025-03-11 02:00:26 +0530

    Refactor check-in API to simplify error handling and improve readability

--- src/app/api/checkIn/[userId]/route.ts
+++ src/app/api/checkIn/[userId]/route.ts
@@ -8,56 +8,49 @@ interface Params {
 export async function GET(req: NextRequest, { params }: Params) {
   const { userId } = await params;
 
-  try {
-    const user = await prisma.tpp_2.findUnique({
-      where: { id: userId, seatStatus: 'BOOKED' },
-      select: {
-        id: true,
-        name: true,
-        seatNumber: true,
-        checkInDay1: true,
-        checkInDay1At: true,
-      },
-    });
+  const user = await prisma.tpp_2.findUnique({
+    where: { id: userId, seatStatus: 'BOOKED' },
+    select: {
+      id: true,
+      name: true,
+      seatNumber: true,
+      checkInDay1: true,
+      checkInDay1At: true,
+    },
+  });
 
-    if (!user)
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  if (!user)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
-    return NextResponse.json(user);
-  } catch (error) {
-    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
-  }
+  return NextResponse.json(user);
 }
 
 export async function POST(req: NextRequest, { params }: Params) {
   const { userId } = await params;
-  try {
-    const user = await prisma.tpp_2.findUnique({
-      where: { id: userId, seatStatus: 'BOOKED' },
-      select: { checkInDay1: true, checkInDay1At: true },
-    });
 
-    if (!user)
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  const user = await prisma.tpp_2.findUnique({
+    where: { id: userId, seatStatus: 'BOOKED' },
+    select: { checkInDay1: true, checkInDay1At: true },
+  });
 
-    if (user.checkInDay1) {
-      return NextResponse.json({
-        message: 'User Already Checked In',
-        checkInTime: user.checkInDay1At,
-        status: 400,
-      });
-    }
+  if (!user)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
-    const updateInfo = await prisma.tpp_2.update({
-      where: { id: userId },
-      data: { checkInDay1: true, checkInDay1At: new Date() },
-    });
-
+  if (user.checkInDay1) {
     return NextResponse.json({
-      message: 'Check-In Successful',
-      checkInTime: updateInfo.checkInDay1At,
+      message: 'User Already Checked In',
+      checkInTime: user.checkInDay1At,
+      status: 400,
     });
-  } catch (error) {
-    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
   }
+
+  const updateInfo = await prisma.tpp_2.update({
+    where: { id: userId },
+    data: { checkInDay1: true, checkInDay1At: new Date() },
+  });
+
+  return NextResponse.json({
+    message: 'Check-In Successful',
+    checkInTime: updateInfo.checkInDay1At,
+  });
 }
